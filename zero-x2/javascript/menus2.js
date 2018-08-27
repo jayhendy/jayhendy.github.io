195434d2dd736f3802dc5c77a3fed18dbaddbb52
Jay Henderson <jay.henderson@uwaterloo.ca>
2018-08-27 11:39:46 -0400

    updated the interface with menu conditions

---
+++ zero-x2/javascript/menus2.js
@@ -0,0 +1,79 @@
+// Create the menu with a sub-menu at the bottom.
+const items2 = [
+  'Text',
+  {
+    name: 'Arrows',
+    children: [
+      'Double',
+      'Dashed',
+      'Solid',
+      'Diagonal',
+      'Right-Angle'
+    ]
+  },
+  {
+    name: 'Lines',
+    children: [
+      'Diagonal',
+      'Dashed',
+      'Solid',
+      'Dotted'
+
+    ]
+  },
+  {
+    name: 'Symbols',
+    children: [
+      'Menu',
+      'Alert',
+      'Compose',
+      'Email',
+      'Attach',
+      'Zoom',
+      'Calendar',
+      'Add'
+    ]
+  },
+  {
+    name: 'Elements',
+    children: [
+      'Round-Dotted',
+      'Round-Solid',
+      'Square-Dotted',
+      'Square-Solid'
+      //,'Alert'
+    ]
+  },
+  {
+    name: 'Stickmen',
+    children: [
+      'Woman',
+      'Man',
+      'Accessible',
+      'Baby'
+    ]
+  },
+  {
+    name: 'Emoticons',
+    children: [
+      'Happy',
+      'Sad',
+      'Cry',
+      'Wink',
+      'Angry',
+      'Cool',
+      'Ill',
+      'Yawn'
+    ]
+  },
+  {
+    name: 'Graphs',
+    children: [
+      'Pie',
+      'Increase',
+      'Decrease',
+      'Squiggle'
+
+    ]
+  }
+];
