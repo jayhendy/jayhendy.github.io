0f3160d73bd4056046fb7edbba12b357b5ff5912
Jay Henderson <jay.henderson@uwaterloo.ca>
2018-09-04 12:04:40 -0400

    added variable submenu delays

---
+++ x1_100/javascript/menu_conditions.js
@@ -0,0 +1,281 @@
+/*
+*
+* Defined Marking Menus
+*
+*/
+
+'use strict';
+
+// 4 ITEMS SETUP 1
+const XS_SETUP_1 = [
+	'Spoon',
+    'Bowl',
+    'Cup',
+    'Fork'
+];
+
+// 4 ITEMS SETUP 2
+const XS_SETUP_2 = [
+	'Notes',
+    'Tape',
+    'Pen',
+    'Glue'
+];
+
+// 8 ITEMS SETUP 1
+const SM_SETUP_1 = [
+	'Glue',
+	'Marker',
+	'Label',
+	'Staple',
+	'Pen',
+	'Notes',
+	'Tape',
+	'Eraser'
+];
+
+// 8 ITEMS SETUP 2
+const SM_SETUP_2 = [
+	'Glass',
+	'Plate',
+	'Mug',
+	'Bowl',
+	'Cup',
+	'Knife',
+	'Spoon',
+	'Fork'
+];
+
+// 4x4 ITEMS SETUP 1
+const MD_SETUP_1 = [
+{
+	name: 'Fruits',
+	children: [
+		'Apple',
+		'Orange',
+		'Banana',
+		'Kiwi'
+	]
+},
+{
+	name: 'Vehicles',
+	children: [
+		'Car',
+		'Boat',
+		'Truck',
+		'Airplane'
+	]
+},
+{
+	name: 'Tools',
+	children: [
+		'Wrench',
+		'Hammer',
+		'Nail',
+		'Pliers'
+	]
+},
+{
+	name: 'Animals',
+	children: [
+		'Horse',
+		'Parrot',
+		'Shark',
+		'Lion'
+	]
+}
+];
+
+// 4x4 ITEMS SETUP 2
+const MD_SETUP_2 = [
+{
+	name: 'Instruments',
+	children: [
+		'Guitar',
+		'Tuba',
+		'Piano',
+		'Flute'
+	]
+},
+{
+	name: 'Vegetables',
+	children: [
+		'Carrot',
+		'Broccoli',
+		'Turnip',
+		'Potato'
+	]
+},
+{
+	name: 'Colors',
+	children: [
+		'Red',
+		'Blue',
+		'Green',
+		'Yellow'
+	]
+},
+{
+	name: 'Furniture',
+	children: [
+		'Chair',
+		'Sofa',
+		'Table',
+		'Drawer'
+	]
+}
+];
+
+// 8x8 ITEMS SETUP 1
+const LG_SETUP_1 = [
+	{
+		name: 'Instruments',
+		children: [
+			'Guitar',
+			'Tuba',
+			'Piano',
+			'Flute'
+		]
+	},
+	{
+		name: 'Vegetables',
+		children: [
+			'Carrot',
+			'Broccoli',
+			'Turnip',
+			'Potato'
+		]
+	},
+	{
+		name: 'Colors',
+		children: [
+			'Red',
+			'Blue',
+			'Green',
+			'Yellow'
+		]
+	},
+	{
+		name: 'Furniture',
+		children: [
+			'Chair',
+			'Sofa',
+			'Table',
+			'Drawer'
+		]
+	},
+	{
+		name: 'Clothing',
+		children: [
+			'Shoes',
+			'T-Shirt',
+			'Coat',
+			'Jeans'
+		]
+	},
+	{
+		name: 'Sports',
+		children: [
+			'Hockey',
+			'Soccer',
+			'Basketball',
+			'Tennis'
+		]
+	},
+	{
+		name: 'Dairy',
+		children: [
+			'Cheese',
+			'Milk',
+			'Ice Cream',
+			'Yogurt'
+		]
+	},
+	{
+		name: 'Languages',
+		children: [
+			'English',
+			'French',
+			'Spanish',
+			'German'
+		]
+	}
+];
+
+
+// // 8x8 ITEMS SETUP 2
+// const LG_SETUP_2 = [
+// 	{
+// 		name: 'Fruits',
+// 		children: [
+// 			'Apple',
+// 			'Orange',
+// 			'Banana',
+// 			'Kiwi'
+// 		]
+// 	},
+// 	{
+// 		name: 'Vehicles',
+// 		children: [
+// 			'Car',
+// 			'Boat',
+// 			'Truck',
+// 			'Airplane'
+// 		]
+// 	},
+// 	{
+// 		name: 'Tools',
+// 		children: [
+// 			'Wrench',
+// 			'Hammer',
+// 			'Nail',
+// 			'Pliers'
+// 		]
+// 	},
+// 	{
+// 		name: 'Animals',
+// 		children: [
+// 			'Horse',
+// 			'Parrot',
+// 			'Shark',
+// 			'Lion'
+// 		]
+// 	},
+// 	{
+// 		name: 'Clothing',
+// 		children: [
+// 			'Shoes',
+// 			'T-Shirt',
+// 			'Coat',
+// 			'Jeans'
+// 		]
+// 	},
+// 	{
+// 		name: 'Sports',
+// 		children: [
+// 			'Hockey',
+// 			'Soccer',
+// 			'Basketball',
+// 			'Tennis'
+// 		]
+// 	},
+// 	{
+// 		name: 'Dairy',
+// 		children: [
+// 			'Cheese',
+// 			'Milk',
+// 			'Ice Cream',
+// 			'Yogurt'
+// 		]
+// 	},
+// 	{
+// 		name: 'Languages',
+// 		children: [
+// 			'English',
+// 			'French',
+// 			'Spanish',
+// 			'German'
+// 		]
+// 	}
+// ];
+
---
+++ x1_333/javascript/menu_final.js
@@ -0,0 +1,360 @@
+const MENU_1_4x4_ARRAY = [
+		'Math',
+		'Art',
+		'History',
+		'Pastry',
+		'Fudge',
+		'Shoes',
+		'Coat',
+		'Ocean'
+];
+
+const MENU_2_4x4_ARRAY = [
+		'Ketchup',
+		'Jay',
+		'Dove',
+		'Wrench',
+		'Pliers',
+		'Hockey',
+		'Baseball',
+		'Soccer'
+];
+
+const MENU_1_8x8_ARRAY = [
+		'Truck',
+		'Limo',
+		'Sedan',
+		'Red',
+		'Green',
+		'Mushroom',
+		'Hail',
+		'Cod'
+];
+
+const MENU_2_8x8_ARRAY = [
+		'Engineer',
+		'Nurse',
+		'Accountant',
+		'Beagle',
+		'Daschund',
+		'Guitar',
+		'Spanish',
+		'Lemon'
+];
+
+const MENU_1_4 = [
+	'Keyboard',
+	'Camera',
+	'Speaker',
+	'Laptop'
+];
+
+const MENU_2_4 = [
+	'Computer',
+	'Tablet',
+	'Printer',
+	'Monitor'
+	
+];
+
+const MENU_1_8 = [
+	'Glass',
+	'Plate',
+	'Mug',
+	'Bowl',
+	'Cup',
+	'Knife',
+	'Spoon',
+	'Fork'
+];
+
+const MENU_2_8 = [
+	'Glue',
+	'Marker',
+	'Label',
+	'Staple',
+	'Pen',
+	'Notes',
+	'Tape',
+	'Eraser'
+];
+
+
+const MENU_1_4x4 = [
+{
+	name: 'Subjects',
+	children: [
+		'Math',
+		'Art',
+		'History',
+		'English'
+		
+		
+	]
+},{
+	name: 'Dessert',
+	children: [
+		'Cake',
+		'Donut',
+		'Pastry',
+		'Fudge'
+	]
+},{
+	name: 'Clothing',
+	children: [
+		'Shoes',
+		'T-Shirt',
+		'Coat',
+		'Jeans'
+	]
+},{
+	name: 'Water',
+	children: [
+		'Bay',
+		'Creek',
+		'Pond',
+		'Ocean'
+	]
+}
+];
+
+
+const MENU_2_4x4 = [
+{
+	name: 'Condiments',
+	children: [
+		'Jam',
+		'Mustard',
+		'Butter',
+		'Ketchup'
+	]
+},{
+	name: 'Birds',
+	children: [
+		'Jay',
+		'Dove',
+		'Eagle',
+		'Flamingo'
+	]
+},{
+	name: 'Tools',
+	children: [
+		'Drill',
+		'Wrench',
+		'Hammer',
+		'Pliers'
+	]
+},{
+	name: 'Sports',
+	children: [
+		'Hockey',
+		'Baseball',
+		'Soccer',
+		'Golf'
+	]
+}
+];
+
+
+const MENU_1_8x8 = [
+{
+	name: 'Trees',
+	children: [
+	'Oak',
+	'Elm',
+	'Maple',
+	'Birch',
+	'Spruce',
+	'Willow',
+	'Pine',
+	'Fir'
+	]
+},{
+	name: 'Auto',
+	children: [
+	'Jeep',
+	'Limo',
+	'Hatchback',
+	'Truck',
+	'Van',
+	'Wagon',
+	'Sedan',
+	'Convertible'
+	]
+},{
+	name: 'Buildings',
+	children: [
+	'Airport',
+	'Bakery',
+	'Church',
+	'Hospital',
+	'Stadium',
+	'Museum',
+	'Jail',
+	'Palace'
+	]
+},{
+	name: 'Seating',
+	children: [
+	'Bench',
+	'Ottoman',
+	'Chair',
+	'Sofa',
+	'Futon',
+	'Loveseat',
+	'Couch',
+	'Throne'
+	]
+},{
+	name: 'Colors',
+	children: [
+	'Red',
+	'Blue',
+	'Yellow',
+	'Green',
+	'Purple',
+	'Indigo',
+	'Magenta',
+	'Violet'
+	]
+},{
+	name: 'Vegetables',
+	children: [
+	'Carrot',
+	'Broccoli',
+	'Turnip',
+	'Potato',
+	'Spinach',
+	'Asparagus',
+	'Mushroom',
+	'Onion'
+	]
+},{
+	name: 'Weather',
+	children: [
+	'Hail',
+	'Rain',
+	'Snow',
+	'Sun',
+	'Fog',
+	'Clouds',
+	'Thunder',
+	'Lightening'
+	]
+},{
+	name: 'Fish',
+	children: [
+	'Bass',
+	'Cod',
+	'Flounder',
+	'Shark',
+	'Tuna',
+	'Herring',
+	'Marlin',
+	'Anchovy'
+	]
+}
+];
+
+
+const MENU_2_8x8 = [
+{
+	name: 'Languages',
+	children: [
+	'Spanish',
+	'English',
+	'French',
+	'German',
+	'Dutch',
+	'Japanese',
+	'Korean',
+	'Russian'
+	]
+},{
+	name: 'Beverages',
+	children: [
+	'Beer',
+	'Coffee',
+	'Wine',
+	'Juice',
+	'Milkshake',
+	'Soda Pop',
+	'Tea',
+	'Latte'
+	]
+},{
+	name: 'Jobs',
+	children: [
+	'Engineer',
+	'Doctor',
+	'Teacher',
+	'Police',
+	'Professor',
+	'Nurse',
+	'Accountant',
+	'Firefighter'
+	]
+},{
+	name: 'Gems',
+	children: [
+	'Diamond',
+	'Emerald',
+	'Topaz',
+	'Ruby',
+	'Sapphire',
+	'Opal',
+	'Amethyst',
+	'Pearl'
+	]
+},{
+	name: 'Storage',
+	children: [
+	'Drawer',
+	'Cabinet',
+	'Shelf',
+	'Bookcase',
+	'Locker',
+	'Safe',
+	'Trunk',
+	'Cubby'
+	]
+},{
+	name: 'Dogs',
+	children: [
+	'Corgie',
+	'Beagle',
+	'Poodle',
+	'Husky',
+	'Daschund',
+	'Retriever',
+	'Terrier',
+	'Spaniel'
+	]
+},{
+	name: 'Fruit',
+	children: [
+	'Apple',
+	'Banana',
+	'Kiwi',
+	'Raspberry',
+	'Lemon',
+	'Peach',
+	'Mango',
+	'Grape'
+	]
+},{
+	name: 'Music',
+	children: [
+	'Piano',
+	'Flute',
+	'Tuba',
+	'Violin',
+	'Cello',
+	'Guitar',
+	'Horn',
+	'Drumkit'
+	]
+}
+];
+
+
